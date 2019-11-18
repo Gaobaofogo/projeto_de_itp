a0e199b9695ad6452d5d62048bd3299f70e62104
GABRIEL AUGUSTO NUNES NOBREGA DINIZ <gaobaofogo@A204-08L.imd.local>
2019-11-18 15:59:13 -0300

    Adicionada verificação de linha para as operações

--- src/manipulacao_arquivos/escrita_de_arquivo.c
+++ src/manipulacao_arquivos/escrita_de_arquivo.c
@@ -9,25 +9,20 @@ typedef struct{
     int b;
 } Pixel;
 
-/*seria chamar no parametro uma matriz numa struct(?? kkk) com a
-a entrada tambem das duas dimensoes X e Y */
-void cor(Pixel **matriz, int dimX, int dimY){
-    int i, j;
-
-    for (i = 0; i < dimX; i++){
-        for (j = 0; j < dimY; j++){
-        /*a 'formula' de um intervalo de random: a+(rand())%(b-a)
-        no nosso caso, a = 0 e b = 255 */
-            matriz[i][j].r = 0+(rand())%255;
-            matriz[i][j].g = 0+(rand())%255;
-            matriz[i][j].b = 0+(rand())%255;
-        }
-    }
-}
-
+/*
+ * clear: Preenche toda a imagem com uma cor especificada em rgb
+ * parametro **image: Matriz de pixels que representa a imagem a ser gerada
+ * parametro largura: largura da imagem
+ * parametro altura: altura da imagem
+ * parametro r: Valor r do formato RGB
+ * parametro g: Valor g do formato RGB
+ * parametro b: Valor b do formato RGB
+ * retorno: Devolve por referência a matriz de pixels pintada com a nova cor especificada
+ */
 void clear(Pixel **image, int largura, int altura, int r, int b, int g){
     int i, j;
 
+    /* Seta todas os pixels com a cor rgb especificada */
     for(i = 0; i < largura; i++){
         for(j = 0; j < altura; j++){
             image[i][j].r = r;
@@ -43,8 +38,18 @@ void desenhaImagem(char nome[], Operacoes operacoes){
     int i, j, k, w;
     Pixel **image;
 
+    /* 
+    Este bloco irá percorrer todas as operações lidas no arquivo e vai tomar
+    a ação descrita na operação
+    */
     for(k = 0; k < operacoes.qntdOperacoes; k++){
+
+        /* Uma sequência de if/else para decidir qual é a operação que o for está varrendo no momento */
         if(strcmp("image", operacoes.operacoes[k].operacao) == 0){
+            /*
+            Atribui as dimensões da imagem para dimX e dimY e aloca o espaço necessário para a imagem
+            como uma matriz de pixels
+            */
             dimX = atoi(operacoes.operacoes[k].parametros[0]);
             dimY = atoi(operacoes.operacoes[k].parametros[1]);
 
@@ -54,22 +59,21 @@ void desenhaImagem(char nome[], Operacoes operacoes){
                 image[j] = (Pixel *) malloc(dimY * sizeof(Pixel));
             }
         } else if(strcmp("clear", operacoes.operacoes[k].operacao) == 0){
+            /* Pega os valores rgb e preenche toda a imagem com a cor desejada */
             int r = atoi(operacoes.operacoes[k].parametros[0]);
             int g = atoi(operacoes.operacoes[k].parametros[1]);
             int b = atoi(operacoes.operacoes[k].parametros[2]);
             clear(image, dimX, dimY, r, g, b);
+        } else if(strcmp("line", operacoes.operacoes[k].operacao) == 0) {
+            /* Código para inserir linha deve estar aqui*/
         }
     }
 
-    for(i = 0; i < dimX; i++){
-        free(image[i]);
-    }
 
-    free(image);
-
-    /*FILE *arquivo;
-
+    /* Escrita de arquivo */
+    FILE *arquivo;
     arquivo = fopen(nome, "wb");
+
     fprintf(arquivo, "P3 \n");
     fprintf(arquivo, "%d %d \n", dimX, dimY);
     fprintf(arquivo, "255 \n");
@@ -82,5 +86,12 @@ void desenhaImagem(char nome[], Operacoes operacoes){
         }
     }
     
-    fclose(arquivo); */
+    fclose(arquivo);
+
+    /* Liberação de memória dos pixels */
+    for(i = 0; i < dimX; i++){
+        free(image[i]);
+    }
+
+    free(image);
 }
