4c94cb14db05850ea99768960e2e2118389ea6a4
LUCAS AUGUSTO DE SOUZA FERREIRA CUNHA DE CARVALHO <lucasaugusto0910@A204-07L.imd.local>
2019-11-18 17:15:12 -0300

    fazendo linhas com erro

--- src/manipulacao_arquivos/escrita_de_arquivo.c
+++ src/manipulacao_arquivos/escrita_de_arquivo.c
@@ -65,9 +65,109 @@ void desenhaImagem(char nome[], Operacoes operacoes){
             int b = atoi(operacoes.operacoes[k].parametros[2]);
             clear(image, dimX, dimY, r, g, b);
         } else if(strcmp("line", operacoes.operacoes[k].operacao) == 0) {
+           
             /* Código para inserir linha deve estar aqui*/
-        }
-    }
+            
+            
+            
+            void swapPixel(int dimX, int dimY, char caracter) {
+                int i, j; 
+                for ( ;i < dimX; ++i){
+                    for (; j < dimY; ++j){
+
+                      if (imagem[i][j]==caracter){ 
+                        printf("\u2588 ");
+                      } else {
+                        printf("%c ", imagem[i][j]);
+                      }
+                    }
+                    printf("\n");
+                  }
+            }
+            int setPoint(int x, int y, char caracter) {
+                image[y][x] = caracter;
+            }
+            int sign(int num) {
+                int result;
+                if(num < 0) {
+                    result = -1;
+                }
+                if(num == 0) {
+                    result = 0;
+                }
+                if(num > 0) {
+                    result = 1;
+                }
+                return(result);
+            }
+            void line(int xi,int yi,int xf,int yf) {
+                
+                int d, dx, dy, temp, x, y, i, s1, s2, swap;
+
+                x = xi;
+                y = yi;
+                dx = abs(xf - xi);
+                dy = abs(yf - yi);
+                s1 = sign(xf - xi);
+                s2 = sign(yf - yf);
+
+                swap = 0;
+                if(dy>dx) {
+                    temp = dx;
+                    dx = dy;
+                    dy = temp;
+                    swap = 1;
+                }
+                else {
+                    swap = 0;
+                }
+                d = 2*dy-dx;
+                for(i = 0; i <= dx; i++) {
+                    setPoint(x, y, '#');
+                    if(d >= 0) {
+                        if(swap == 1) {
+                            x = x+s1;
+                        }
+                        else {
+                            y = y+s2;
+                        }
+                        d = d-(2*dx);
+                    }
+                    if(swap ==1) {
+                        y = y+s2;
+                    }
+                    else {
+                        x = x+s1;
+                    }
+                    d = d+(2*dy);
+                }
+                setPoint(x, y, '#');
+            }
+            void line_b(int x1, int y1, int x2, int y2) {
+                int dx, dy, incrementoE, incrementeNE, d, x, y;
+
+                x = x1;
+                y = y1;
+                dx = x2 - x1;
+                dy = y2 - y1;
+                d = 2*dy - dx;
+                incrementoE = 2*dy;
+                incrementeNE = 2*(dy-dx);
+
+                setPoint(x, y, '#');
+                while(x < x1) {
+                    if(d<=0) {
+                        d = d + incrementoE;
+                        x = x+1;
+                    }
+                    else {
+                        d = d + incrementeNE;
+                        x = x +1;
+                        y = y + 1;
+                    }
+                }
+                setPoint(x, y, '#');
+            }
 
 
     /* Escrita de arquivo */
@@ -94,4 +194,5 @@ void desenhaImagem(char nome[], Operacoes operacoes){
     }
 
     free(image);
+}
 }
