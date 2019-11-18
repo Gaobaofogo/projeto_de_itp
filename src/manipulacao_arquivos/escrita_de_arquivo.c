7c9bd4dcdb3dc7df5ed59624b607f9c52a16a3be
LUCAS AUGUSTO DE SOUZA FERREIRA CUNHA DE CARVALHO <lucasaugusto0910@A204-07L.imd.local>
2019-11-18 18:42:18 -0300

    fazendo linhas com erros ainda

--- src/manipulacao_arquivos/escrita_de_arquivo.c
+++ src/manipulacao_arquivos/escrita_de_arquivo.c
@@ -25,11 +25,71 @@ void clear(Pixel **image, int largura, int altura, int r, int b, int g){
     /* Seta todas os pixels com a cor rgb especificada */
     for(i = 0; i < largura; i++){
         for(j = 0; j < altura; j++){
-            image[i][j].r = r;
-            image[i][j].g = g;
-            image[i][j].b = b;
+            imagem[i][j].r = r;
+            imagem[i][j].g = g;
+            imagem[i][j].b = b;
         }
     }
+}
+
+void swapPixel(int dimX, int dimY, int caracter1, int caracter2, int caracter3) {
+    int i, j; 
+    int imagem[i][j];
+    for ( ;i < dimX; ++i){
+        for (; j < dimY; ++j){
+
+          if (imagem[i][j]==caracter){ 
+            printf("\u2588 ");
+          } else {
+            printf("%c ", imagem[i][j]);
+          }
+        }
+        printf("\n");
+      }
+}
+int setPoint(int x, int y, int caracter1,int caracter2, int caracter3) {
+    imagem[x][y].r = caracter1;
+    imagem[x][y].g = caracter2;
+    imagem[x][y].b = caracter3;
+}
+int sign(int num) {
+    int result;
+    if(num < 0) {
+        result = -1;
+    }
+    if(num == 0) {
+        result = 0;
+    }
+    if(num > 0) {
+        result = 1;
+    }
+    return(result);
+}
+
+void line_b(int x1, int y1, int x2, int y2) {
+    int dx, dy, incrementoE, incrementeNE, d, x, y;
+
+    x = x1;
+    y = y1;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    d = 2*dy - dx;
+    incrementoE = 2*dy;
+    incrementeNE = 2*(dy-dx);
+
+    setPoint(x, y, 0,0,0);
+    while(x < x1) {
+        if(d<=0) {
+            d = d + incrementoE;
+            x = x+1;
+        }
+        else {
+            d = d + incrementeNE;
+            x = x +1;
+            y = y + 1;
+        }
+    }
+    setPoint(x, y, 0,0,0);
 }
 
 /*Operacoes operacoes*/
@@ -43,7 +103,6 @@ void desenhaImagem(char nome[], Operacoes operacoes){
     a ação descrita na operação
     */
     for(k = 0; k < operacoes.qntdOperacoes; k++){
-
         /* Uma sequência de if/else para decidir qual é a operação que o for está varrendo no momento */
         if(strcmp("image", operacoes.operacoes[k].operacao) == 0){
             /*
@@ -67,133 +126,33 @@ void desenhaImagem(char nome[], Operacoes operacoes){
         } else if(strcmp("line", operacoes.operacoes[k].operacao) == 0) {
            
             /* Código para inserir linha deve estar aqui*/
-            
-            
-            
-            void swapPixel(int dimX, int dimY, char caracter) {
-                int i, j; 
-                int imagem[i][j];
-                for ( ;i < dimX; ++i){
-                    for (; j < dimY; ++j){
+            line( 2,6,5,5);
 
-                      if (imagem[i][j]==caracter){ 
-                        printf("\u2588 ");
-                      } else {
-                        printf("%c ", imagem[i][j]);
-                      }
-                    }
-                    printf("\n");
-                  }
-            }
-            int setPoint(int x, int y, char caracter) {
-                image[x][y] = caracter;
-            }
-            int sign(int num) {
-                int result;
-                if(num < 0) {
-                    result = -1;
-                }
-                if(num == 0) {
-                    result = 0;
-                }
-                if(num > 0) {
-                    result = 1;
-                }
-                return(result);
-            }
-            void line(int xi,int yi,int xf,int yf) {
-                
-                int d, dx, dy, temp, x, y, i, s1, s2, swap;
-
-                x = xi;
-                y = yi;
-                dx = abs(xf - xi);
-                dy = abs(yf - yi);
-                s1 = sign(xf - xi);
-                s2 = sign(yf - yf);
-
-                swap = 0;
-                if(dy>dx) {
-                    temp = dx;
-                    dx = dy;
-                    dy = temp;
-                    swap = 1;
-                }
-                else {
-                    swap = 0;
-                }
-                d = 2*dy-dx;
-                for(i = 0; i <= dx; i++) {
-                    setPoint(x, y, '#');
-                    if(d >= 0) {
-                        if(swap == 1) {
-                            x = x+s1;
-                        }
-                        else {
-                            y = y+s2;
-                        }
-                        d = d-(2*dx);
-                    }
-                    if(swap ==1) {
-                        y = y+s2;
-                    }
-                    else {
-                        x = x+s1;
-                    }
-                    d = d+(2*dy);
-                }
-                setPoint(x, y, '#');
-            }
-            void line_b(int x1, int y1, int x2, int y2) {
-                int dx, dy, incrementoE, incrementeNE, d, x, y;
-
-                x = x1;
-                y = y1;
-                dx = x2 - x1;
-                dy = y2 - y1;
-                d = 2*dy - dx;
-                incrementoE = 2*dy;
-                incrementeNE = 2*(dy-dx);
-
-                setPoint(x, y, '#');
-                while(x < x1) {
-                    if(d<=0) {
-                        d = d + incrementoE;
-                        x = x+1;
-                    }
-                    else {
-                        d = d + incrementeNE;
-                        x = x +1;
-                        y = y + 1;
-                    }
-                }
-                setPoint(x, y, '#');
-            }
-
-
-        /* Escrita de arquivo */
-        FILE *arquivo;
-        arquivo = fopen(nome, "wb");
-
-        fprintf(arquivo, "P3 \n");
-        fprintf(arquivo, "%d %d \n", dimX, dimY);
-        fprintf(arquivo, "255 \n");
-        
-        for (i = 0; i < dimX; i++){
-            for (j = 0; j < dimY; j++){
-                fprintf(arquivo, "%i ", image[i][j].r);
-                fprintf(arquivo, "%i ", image[i][j].g);
-                fprintf(arquivo, "%i \n", image[i][j].b);
-            }
+            swapPixel(10, 10, 255, 255, 255);
         }
-        
-        fclose(arquivo);
-
-        /* Liberação de memória dos pixels */
-        for(i = 0; i < dimX; i++){
-            free(image[i]);
-        }
-        free(image);
     }
-}
+
+    /* Escrita de arquivo */
+    FILE *arquivo;
+    arquivo = fopen(nome, "wb");
+
+    fprintf(arquivo, "P3 \n");
+    fprintf(arquivo, "%d %d \n", dimX, dimY);
+    fprintf(arquivo, "255 \n");
+    
+    for (i = 0; i < dimX; i++){
+        for (j = 0; j < dimY; j++){
+            fprintf(arquivo, "%i ", image[i][j].r);
+            fprintf(arquivo, "%i ", image[i][j].g);
+            fprintf(arquivo, "%i \n", image[i][j].b);
+        }
+    }
+    
+    fclose(arquivo);
+
+    /* Liberação de memória dos pixels */
+    for(i = 0; i < dimX; i++){
+        free(image[i]);
+    }
+    free(image);
 }
